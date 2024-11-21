329620f8391945ac5d6f5f76d7757626b99da3d1
RemousArkarou <dds0022@edu.rocwb.nl>
2024-11-21 12:36:54 +0100

    some small changes

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -2,11 +2,13 @@
 <head>
     <title></title>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="{{ asset('css/stytle.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css">
 </head>
 <body>
     <header>
+
         <h1>School Football App</h1>
+        <img src="url{{ asset('img/SFA Logo.png') }}" alt="">
         <nav>
             <x-navbar/>
         </nav>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,8 +16,9 @@
                 <p>SFA is a simple app where you can see football matches of your schools, bet on the matches and gain digital currency</p>
                 <p>if your bet is correct. You can also make your own team and sign up to actually play the football matches and represent</p>
                 <p>for your team and maybe even school. There are tournaments that can only be started by an Admin. In these tournaments all</p>
-                <p>the different teams. When your team wins youget a point, the team with the most points wins and stands number one on the</p>
-                <p>leaderboard. The refferee will add the scores of the latest match and tournament to the app for all to see</p>
+                <p>the different teams go against all the other teams. So everyone goes against everyone, keeping it fair and simple. When your</p>
+                <p>team wins youget a point, the team with the most points wins and stands number one on the leaderboard. The refferee will add</p>
+                <p>the scores of the latest match and tournament to the app for all to see</p>
             </main>
             <footer>
 
